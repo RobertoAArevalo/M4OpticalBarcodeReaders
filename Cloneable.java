01493028517c19b01a06b420c20eb5b8eb7fe760
Ali Bakr <abakr@csumb.edu>
2022-03-27 17:56:40 -0700

    added Cloneable interface

---
+++ Cloneable.java
@@ -0,0 +1,8 @@
+public interface Cloneable
+{
+   boolean getPixel(int row, int col);
+   boolean setPixel(int row, int col, boolean value);
+   void checkSize(String[] data);
+   void displayToConsole();
+   Object clone();
+}
