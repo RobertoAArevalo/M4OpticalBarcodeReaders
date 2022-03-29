c6268e5e161ba31103d8bb7cbf7bf3f3ce59ca36
Roberto Arevalo <roberttt44@hotmail.com>
2022-03-28 19:05:42 -0700

    edits

--- BarcodeRead.java
+++ BarcodeRead.java
@@ -180,22 +180,74 @@ interface BarcodeIO
     }
     public boolean generateImageFromText()
     {
-        
+       actualWidt = this.text.length() + 2; //don't forget the border
+       actualHeight = 10; // 8 bits, plus the spine
+       this.clearImage();
+       // add borders to image
+       //set top and bottom border
+       for (int i = 0; i < actualWidt; i++)
+       {
+          this.image.setPixel(image.MAX_HEIGHT - 1, i, true);
+          if (i % 2 == 0)
+             this.image.setPixel(image.MAX_HEIGHT - this.actualHeight, i, true);
+       }
+       //set left and right border
+       for (int i = 0; i < 10; i++)
+       {
+          this.image.setPixel(image.MAX_HEIGHT - this.actualHeight + i, 0, true);
+          if (i % 2 == 0)
+             this.image.setPixel(image.MAX_HEIGHT - this.actualHeight + i,
+                   this.actualWidt - 1, true);
+       }
+
+       //get ascii value of string
+       for (int index = 0; index < text.length(); index++)
+          writeCharToCol(index, (int) text.charAt(index)); //write to array col
+
+       return true;
     }
-    public boolean translateImageToText()
-    {
-        
-    }
-    public void displayTextToConsole()
-    {
-        
-    }
-    public void displayImageToConsole()
-    {
-        
-    }
+
+   public boolean translateImageToText()
+   {
+      String imageText = "  ";
+      for (int col = 1; col < getActualWidth() - 1; col++)
+         imageText += readCharFromCol(col);
+
+      //set text to object
+      this.text = imageText;
+      return true;
+   }
+   public void displayTextToConsole()
+   {
+      System.out.println(this.text);
+   }
+   public void displayImageToConsole()
+   {
+      String dash = "-";
+      //print top border
+      System.out.print(dash.repeat(getActualWidth() + 2) + "\n");
+
+      for (int row = (image.MAX_HEIGHT - getActualHeight());
+           row < image.MAX_HEIGHT; row++)
+      {
+         //print left side border
+         System.out.print("|");
+         for (int col = 0; col < getActualWidth(); col++)
+            if (image.getPixel(row, col) == true)
+               System.out.print(BLACK_CHAR);
+            else
+               System.out.print(WHITE_CHAR);
+
+         //print right side border and move to next line
+         System.out.print("|\n");
+
+      }
+      //print bottom border
+      System.out.print(dash.repeat(getActualWidth() + 2) + "\n");
+   }
     
 }
+
 class DataMatrix implements BarcodeIO
 {
     public static final char BLACK_CHAR = '*';
@@ -204,7 +256,19 @@ class DataMatrix implements BarcodeIO
     private String text = " ";
     private int actualWidt = 0; 
     private int actualHeight = 0;
-    
+
+   DataMatrix(****)
+   {
+      {
+         imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
+         for(int i = 0; i < MAX_HEIGHT; i++)
+            for(int j = 0; j < MAX_WIDTH; j++)
+               imageData[i][j] = false;
+      }
+   }
+
+   }
+
     DataMatrix(BarcodeImage image)
     {
         
